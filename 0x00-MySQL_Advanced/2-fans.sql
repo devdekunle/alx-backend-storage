12a664d1bb93413b68d687d60081738efee9fd73
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-07-13 19:46:30 +0100

    count number of fans for each country origins

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -6,4 +6,4 @@
 -- Column names must be: origin and nb_fans
 -- Your script can be executed on any database
 
-SELECT origin, COUNT(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
