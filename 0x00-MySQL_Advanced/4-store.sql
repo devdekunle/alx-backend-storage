f30f99dc2993302ecc2ba8d632ed64c7227cea8d
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-07-14 21:42:07 +0100

    create trigger event after insert on a table

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- Write a SQL script that creates a trigger
+-- that decreases the quantity of an item after adding a new order.
+
+-- Quantity in the table items can be negative.
+
+CREATE TRIGGER decrs_item AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name
