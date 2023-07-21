e4a3d38a4e993e16d57a5aff312c47730703c4ba
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-07-21 13:14:35 -0700

    update a mongodb document

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+ A Python function that changes all topics of a school
+ document based on the name:
+
+Prototype: def update_topics(mongo_collection, name, topics):
+mongo_collection will be the pymongo collection object
+name (string) will be the school name to update
+topics (list of strings) will be the list of
+topics approached in the school
+"""
+
+def update_topics(mongo_collection, name, topics):
+        """
+        updates a document
+        """
+        mongo_collection.update_many(
+            {"name": name},
+            {"$set": {"topics": topics}})
